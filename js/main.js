bb8f82c0faecab9346acf2ec9396767a85237c9e
Joel D Souza <joel.dsouza@kapturecrm.com>
2020-05-13 01:45:48 +0530

    Added counters

--- js/main.js
+++ js/main.js
@@ -2,21 +2,36 @@
  * @author joeldsouza
  */
 
- var GameEngine = {
-    init: function(difficulty="medium") {
+ function GameEngine (){
+    this.init = function(difficulty="medium") {
+        
         // Initialize
         // Default
         this.getDifficulty(difficulty)
         this.m = 5;
         this.n = 5;
-        this.minColoredSquare = 3;
         this.maxTime = 10000; // Total ms to finish a game.
+        this.totalSquares = this.m * this.n;
+        this.minColoredSquare = 3;
+
+        // Get elements 
         this.gameArena = document.getElementById("arena")
         this.modalEl = document.getElementById("options-modal")
-        this.startGame()
-    },
-    getDifficulty : function(difficulty) {
+        this.counterEl = document.getElementById("counter")
 
+        this.registerEvents()
+
+        // Creates a new game
+        this.generateSquares()
+    };
+    /**
+     * Returns the grid size based on a difficulty level.
+     *
+     * @param {String} difficulty
+     * @returns {Array} gridSize
+     */
+    this.getDifficulty = function(difficulty) {
+        
         switch (difficulty) {
             case 'easy':
                 return [3,3]
@@ -27,19 +42,27 @@
             default:
                 return [5,5]
         }
-    },
-    renderGame : function() {
+    };
+    /**
+     *
+     *
+     */
+    this.renderGame = function() {
+        
         // Render the changes to the dom.
         let el = document.getElementById("game-app")
         el.innerHTML = "Game renders here"
-
-    },
-    startGame: function() {
+    };
+    /**
+     *  Generates red sqaures.
+     *
+     */
+    this.generateSquares = function() {
+        
         // The brain of the game.
-        this.totalSquares = this.m * this.n;
         this.coloredSquares = [];
-        this.resetGame()
-        //Get random squares to be red.
+
+        //Randomly select the red squares.
         for(let start=0; start<=this.totalSquares/2; start++) {
 
             this.coloredSquares = [
@@ -49,15 +72,21 @@
         }
 
         this.renderLayout()
-     },
-     renderLayout: function() {
+    };
+
+    /**
+     * Generates and Renders the grid layout.
+     *
+     */
+    this.renderLayout = function() {
+        
         // Render the game layout
         let boxEl = ''
         for(let start=0; start<=this.totalSquares; start++) {
             boxEl += `<div
                         class="box ${this.coloredSquares.includes(start) ? 'red' : 'blue'}"
                         data-val="${start}"
-                      ></div>`
+                    ></div>`
         }
 
         // Calculate the dimensions of each box i.e: (Total )
@@ -66,8 +95,16 @@
         this.gameArena.style.gridTemplateRows = `repeat(${this.m}, ${boxDim}px)`
         this.gameArena.style.gridTemplateColumns = `repeat(${this.n}, ${boxDim}px)`
         this.gameArena.innerHTML = boxEl;
-     },
-     showOptions: function() {
+
+        this.startGame()
+    };
+
+    /**
+     * Displays the option modal.
+     *
+     */
+    this.showOptions = function() {
+        
         //  Show the options modal
         let messageModal = `<div class="modal-wrapper">
                             <span>${message}</span>
@@ -75,25 +112,51 @@
 
         let restartModal = `<div class=""></div>`
 
-     },
-     resetGame: function(){
+    };
+
+    this.startCountdown = function() {
+
+        clearTimeout(this.freezeTimeout)
+
+        var countdown = 10
+        this.counterListener = setInterval(() => {
+            this.counterEl.innerText = countdown;
+            countdown =countdown-1;
+        }, 1000);
+
+        this.gameCountDown = setTimeout(()=>{
+            this.resetGame()
+        }, this.maxTime)
+    };
+
+    this.resetGame = function(){
         // Clears the layout, Resets all counters.
-        this.gameArena.innerHTML="";
-     },
-     handleCounter: function() {
+        
+        clearInterval(this.counterListener)
+        clearTimeout(this.gameCountDown)
+        this.counterListener = 0
+    };
+
+    this.startGame = function() {
         // Handles the game countdown.
-        setTimeout(() => {
-            
-        }, this.maxTime);
-     },
-     showModal: function(modal) {
+        this.freezeTimeout = setTimeout(() => {
+            // this.clearDashboard()
+            this.startCountdown()
+        }, 1000);
+    };
+    
+    this.showModal = function(modal) {
         
         this.modalEl.innerHTML(modal)
-     },
-};
+    };
+
+    this.registerEvents = function() {
+        // Register all events here.
+        let restartBtn = document.getElementById("restart-btn")
+        restartBtn.addEventListener("click", () => this.resetGame())
+    };
+}
 
 // Load the game
-GameEngine.init("medium")
-
-var restartBtn = document.getElementById("restart-btn")
-restartBtn.addEventListener("click", ()=>{GameEngine.init()})
+var GameInst = new GameEngine()
+GameInst.init("medium")
