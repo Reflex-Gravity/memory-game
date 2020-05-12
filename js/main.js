ba9e6559c9e67fb1f9cee18a4a64030c802da0bc
Joel D Souza <joel.dsouza@kapturecrm.com>
2020-05-12 22:17:43 +0530

    Created boxes

--- js/main.js
+++ js/main.js
@@ -2,34 +2,90 @@
  * @author joeldsouza
  */
 
- (function(){
+ var GameEngine = {
+    init: function(difficulty) {
+        // Initialize
 
+        // Default
+        this.getDifficulty(difficulty)
+        this.m = 5;
+        this.n = 5;
+        this.minColoredSquare = 3;
+        this.maxTime = 10;
+        this.gameArena = document.getElementById("arena")
+        this.modalEl = document.getElementById("options-modal")
+        this.startGame()
+    },
+    getDifficulty : function(difficulty) {
 
-     // Render the changes to the dom.
-     var renderGame = function() {
-         
-     
-     
-         let el = document.getElementById("game-app")
-         el.innerHTML = "Game reders here"
-     }
-     
-     // Define GameEngine
-     var gameEngine = function(m, n) {
+        switch (difficulty) {
+            case 'easy':
+                return [3,3]
+            case 'medium':
+                return [5,5]
+            case 'hard':
+                return [6,6]
+            default:
+                return [5,5]
+        }
+    },
+    renderGame : function() {
+        // Render the changes to the dom.
+        let el = document.getElementById("game-app")
+        el.innerHTML = "Game renders here"
 
-        const s = 10; // 10 seconds to guess.
+    },
+    startGame: function() {
+        // The brain of the game.
+        this.totalSquares = this.m * this.n;
+        this.coloredSquares = [];
+        //Get random squares to be red.
+        for(let start=0; start<=this.totalSquares; start++) {
 
-        const r = 10;
-     
-     }
+            this.coloredSquares = [
+                ...this.coloredSquares,
+                Math.floor(Math.random() * (this.totalSquares/2 - this.minColoredSquare)) + this.minColoredSquare
+            ];
+        }
 
-     function renderLayout() {
+        this.renderLayout()
+     },
+     renderLayout: function() {
+        // Render the game layout
+        let boxEl = ''
+        for(let start=0; start<=this.totalSquares; start++) {
+            boxEl += `<div class="box ${this.coloredSquares.includes(start) ? 'red' : 'blue'}"></div>`
+        }
 
-     }
-     
-     
-     var Game = new gameEngine(5,5)
-     
-     renderGame()
- }
- )()
+        const boxDim = (this.gameArena.clientWidth - (10*(this.m-1)))/this.m
+        
+        this.gameArena.style.gridTemplateRows = `repeat(${this.m}, ${boxDim}px)`
+        this.gameArena.style.gridTemplateColumns = `repeat(${this.n}, ${boxDim}px)`
+        this.gameArena.innerHTML = boxEl;
+     },
+     showOptions: function() {
+        //  Show the options modal
+        let messageModal = `<div class="modal-wrapper">
+                            <span>${message}</span>
+                        </div>`;
+
+        let restartModal = `<div class=""></div>`
+
+     },
+     clearGame: function(){
+        // Clears the layout.
+        this.gameArena.innerHTML="";
+     },
+     showModal: function(modal) {
+        
+        this.modalEl.innerHTML(modal)
+     },
+     registerEvents: function() {
+        //  Register all the events
+        document.addEventListener("click", renderOptions)
+        document.addEventListener("click")
+     } 
+};
+
+// Load the game
+GameEngine.init("medium")
